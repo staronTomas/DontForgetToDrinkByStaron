0e2fe77ec762b43ffe0780174ccd7d7217668237
staronTomas <tomas.staronn@gmail.com>
2022-04-13 18:33:46 +0200

    ...

---
+++ app/src/main/java/com/example/dont_forget_to_drink/FirstTimeStartedActivity.kt
@@ -0,0 +1,11 @@
+package com.example.dont_forget_to_drink
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FirstTimeStartedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_time_started)
+    }
+}
--- app/src/main/java/com/example/dont_forget_to_drink/MainActivity.kt
+++ app/src/main/java/com/example/dont_forget_to_drink/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.dont_forget_to_drink
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 
@@ -13,8 +15,24 @@ class MainActivity : AppCompatActivity() {
     private lateinit var toggle : ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
+        val firstrun = getSharedPreferences("PREFERENCE", MODE_PRIVATE).getBoolean("firstrun", true)
+        if (firstrun) {
+            //... Display the dialog message here ...
+            // Save the state
+            getSharedPreferences("PREFERENCE", MODE_PRIVATE)
+                .edit()
+                .putBoolean("firstrun", false)
+                .commit()
+
+            showFirstStartWindow()
+        }
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
 
 
         val drawerLayout: DrawerLayout = findViewById(R.id.drawerLayout)
@@ -33,7 +51,6 @@ class MainActivity : AppCompatActivity() {
                 R.id.nav_statistics -> Toast.makeText(applicationContext, "Clicked Statistics", Toast.LENGTH_SHORT).show()
                 R.id.nav_disable_alarm -> Toast.makeText(applicationContext, "Clicked Disable Alarm", Toast.LENGTH_SHORT).show()
                 R.id.nav_settings -> Toast.makeText(applicationContext, "Clicked Settings", Toast.LENGTH_SHORT).show()
-                R.id.nav_login -> Toast.makeText(applicationContext, "Clicked Login", Toast.LENGTH_SHORT).show()
                 R.id.nav_share -> Toast.makeText(applicationContext, "Clicked Share", Toast.LENGTH_SHORT).show()
                 R.id.nav_rate_us -> Toast.makeText(applicationContext, "Clicked Rate us", Toast.LENGTH_SHORT).show()
 
@@ -41,6 +58,11 @@ class MainActivity : AppCompatActivity() {
 
             true
         }
+    }
+
+    private fun showFirstStartWindow() {
+        val intent = Intent(this, FirstTimeStartedActivity::class.java)
+        startActivity(intent)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
