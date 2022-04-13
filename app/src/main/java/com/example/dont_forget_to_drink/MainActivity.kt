2bbf5dd17920e5c8861f856d4d654f115cd0f0b6
staronTomas <tomas.staronn@gmail.com>
2022-04-13 13:55:17 +0200

    Initial commit

---
+++ app/src/main/java/com/example/dont_forget_to_drink/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.dont_forget_to_drink
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
