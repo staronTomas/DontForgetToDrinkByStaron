77245cf4411afbc5ee9576ea7c3173fbd3a54507
staronTomas <tomas.staronn@gmail.com>
2022-04-15 14:33:44 +0200

    ...

--- app/src/main/java/com/example/dont_forget_to_drink/FirstTimeStartedActivity.kt
+++ app/src/main/java/com/example/dont_forget_to_drink/FirstTimeStartedActivity.kt
@@ -1,18 +1,49 @@
 package com.example.dont_forget_to_drink
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.ImageView
-import android.widget.RadioButton
+import android.widget.*
 
 class FirstTimeStartedActivity : AppCompatActivity() {
 
 
+    lateinit var name : EditText
+    lateinit var surname : EditText
+    lateinit var button : Button
+
+    lateinit var sp : SharedPreferences
+    lateinit var nameStr : String
+    lateinit var surnameStr : String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first_time_started)
+
+        name = findViewById(R.id.userName)
+        surname = findViewById(R.id.userSurname)
+
+
+        sp = getSharedPreferences("MyUserPrefs", Context.MODE_PRIVATE)
+
+        button.setOnClickListener(View.OnClickListener {
+            @Override
+            fun onClick(view: View) {
+                nameStr = name.text.toString()
+                surnameStr = surname.text.toString()
+
+                var editor: SharedPreferences.Editor = sp.edit()
+
+                editor.putString("userName", nameStr)
+                editor.putString("userSurname", surnameStr)
+
+                editor.commit()
+                Toast.makeText(this@FirstTimeStartedActivity, "Information Saved", Toast.LENGTH_SHORT).show()
+            }
+        })
+
     }
 
 
