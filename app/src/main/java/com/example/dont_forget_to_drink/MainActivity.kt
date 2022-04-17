d02fa3d9356e5d9421fcb94e4aeef194c73c32ec
staronTomas <tomas.staronn@gmail.com>
2022-04-17 14:05:47 +0200

    ...

--- app/src/main/java/com/example/dont_forget_to_drink/MainActivity.kt
+++ app/src/main/java/com/example/dont_forget_to_drink/MainActivity.kt
@@ -21,8 +21,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var userName: TextView
     private var mMediaPlayer: MediaPlayer? = null
 
-    private var asdas: Int = 0
-
     override fun onCreate(savedInstanceState: Bundle?) {
 
 
