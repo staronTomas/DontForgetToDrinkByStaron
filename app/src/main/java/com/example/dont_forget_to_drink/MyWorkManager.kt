383dac076a453bccd95108868c0694fa4ef1aaee
Tomáš Staroň <tomas.staronn@gmail.com>
2022-06-06 22:22:15 +0200

    Notifikacie urobene, funguju kazdu hodinu

--- app/src/main/java/com/example/dont_forget_to_drink/MyWorkManager.kt
+++ app/src/main/java/com/example/dont_forget_to_drink/MyWorkManager.kt
@@ -4,6 +4,7 @@ import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_MUTABLE
 import android.content.Context
 import android.content.Intent
 import android.os.Build
@@ -43,7 +44,7 @@ class MyWorkManager(context: Context, workerParameters: WorkerParameters) :
 
         val pendingIntent = PendingIntent.getActivity(
             applicationContext,
-            0, intent, 0
+            0, intent, PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
         )
 
         val notification = NotificationCompat.Builder(
